794aad4b5f67fad9dad20b7a6ac8d4bde5721435
Lukas Stix <lukas.stix2001@gmail.com>
2019-12-13 09:13:34 +0000

    fds

--- GPIO_G3uebung/main.c
+++ GPIO_G3uebung/main.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "functions.h"
 
 
@@ -18,6 +15,49 @@ int main (int argc, char *argv[])
 //
 //printf("%s %d",color,hertz);
 //  verkabeltesPiBlink(color,hertz);
+
+//native C
+
+GPIOExport(GPIORED);
+GPIODirection(GPIORED,IN);
+GPIOWrite(GPIORED,HIGH);
+
+/*
+	 * Enable GPIO pins
+	 */
+	if (-1 == GPIOExport(POUT) || -1 == GPIOExport(PIN))
+		return(1);
+
+	/*
+	 * Set GPIO directions
+	 */
+	if (-1 == GPIODirection(POUT, OUT) || -1 == GPIODirection(PIN, IN))
+		return(2);
+
+	do {
+		/*
+		 * Write GPIO value
+		 */
+		if (-1 == GPIOWrite(POUT, repeat % 2))
+			return(3);
+
+		/*
+		 * Read GPIO value
+		 */
+		printf("I'm reading %d in GPIO %d\n", GPIORead(PIN), PIN);
+
+		usleep(500 * 1000);
+	}
+	while (repeat--);
+
+	/*
+	 * Disable GPIO pins
+	 */
+	if (-1 == GPIOUnexport(POUT) || -1 == GPIOUnexport(PIN))
+		return(4);
+
+	return(0);
+
   return 0 ;
 }
 
