721b6a362c4016cc99dcf6fa38bf648b55d84727
Lukas Stix <lukas.stix2001@gmail.com>
2019-12-13 09:13:21 +0000

    fds

--- GPIO_G3uebung/functions.c
+++ GPIO_G3uebung/functions.c
@@ -27,8 +27,7 @@ void verkabeltesPiBlink(char color[],int hertz){
     }
 }
 
-static int
-GPIOExport(int pin)
+int GPIOExport(int pin)
 {
 #define BUFFER_MAX 3
     char buffer[BUFFER_MAX];
@@ -47,8 +46,7 @@ GPIOExport(int pin)
     return(0);
 }
 
-static int
-GPIOUnexport(int pin)
+int GPIOUnexport(int pin)
 {
     char buffer[BUFFER_MAX];
     ssize_t bytes_written;
@@ -66,8 +64,7 @@ GPIOUnexport(int pin)
     return(0);
 }
 
-static int
-GPIODirection(int pin, int dir)
+int GPIODirection(int pin, int dir)
 {
     static const char s_directions_str[]  = "in\0out";
 
@@ -91,8 +88,7 @@ GPIODirection(int pin, int dir)
     return(0);
 }
 
-static int
-GPIORead(int pin)
+int GPIORead(int pin)
 {
 #define VALUE_MAX 30
     char path[VALUE_MAX];
@@ -116,8 +112,7 @@ GPIORead(int pin)
     return(atoi(value_str));
 }
 
-static int
-GPIOWrite(int pin, int value)
+int GPIOWrite(int pin, int value)
 {
     static const char s_values_str[] = "01";
 
